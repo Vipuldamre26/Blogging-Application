426f3a1c4acfcab60c4fe0a48a04c48643e57cea
Vipul <vipuldamrepro26@gmail.com>
2024-08-28 19:57:10 +0530

    add schema and navbar

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: '/images/default.png',
+    }
+    
+})
--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.set(express.json());
 
 // Routes 
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 
 
