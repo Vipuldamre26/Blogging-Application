b64899cafab417afa04c33bfa93fe561e9496c5f
Vipul <vipuldamrepro26@gmail.com>
2024-08-28 17:33:42 +0530

    started working on blog app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const { dbConnection } = require('./connection');
+
+const PORT = 8004;
+
+const app = express();
+
+
+// connection 
+
+dbConnection('mongodb://127.0.0.1:27017/blog-app')
+.then(() => console.log('MongoDB connected'));
+
+
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"))
+
+
+
+// Middlewares 
+
+app.set(express.json());
+
+
+
+// Routes 
+
+
+
+
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
