6eb37116c58952cc9589359b21ed2d3c2bdd875f
Vipul <vipuldamrepro26@gmail.com>
2024-09-26 21:35:03 +0530

    successfully added authentication and integrate multer for file upload

--- routes/blog.js
+++ routes/blog.js
@@ -1,12 +1,37 @@
 const { Router } = require('express');
-
+const multer = require('multer');
+const path = require('path');
 
 const router = Router();
 
+// external library multer for storing files like .png .jpg .pdf etc 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`))
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName);
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+
+
+
 router.get('/add-new', (req, res) => {
+    
     return res.render('addBlog', {
-        user: res.user,
+        user: req.user,
     })
+})
+
+router.post('/', upload.single('coverImage'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    
+    return res.redirect('/');
 })
 
 
