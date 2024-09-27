77ca5f9115439732068f12caafb58db2f3ebe5a6
Vipul <vipuldamrepro26@gmail.com>
2024-09-27 17:36:36 +0530

    added blog to db

--- routes/blog.js
+++ routes/blog.js
@@ -2,36 +2,44 @@ const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
 
+const Blog = require('../models/blog');
+
 const router = Router();
 
 // external library multer for storing files like .png .jpg .pdf etc 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve(`./public/uploads/`))
-    },
-    filename: function (req, file, cb) {
-      const fileName = `${Date.now()}-${file.originalname}`;
-      cb(null, fileName);
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`))
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  }
+})
+
+const upload = multer({ storage: storage })
 
 
 
 
 router.get('/add-new', (req, res) => {
-    
-    return res.render('addBlog', {
-        user: req.user,
-    })
+
+  return res.render('addBlog', {
+    user: req.user,
+  })
 })
 
-router.post('/', upload.single('coverImage'), (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
-    
-    return res.redirect('/');
+router.post('/', upload.single('coverImage'), async (req, res) => {
+  const { title, body } = req.body;
+
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`
+  })
+
+  return res.redirect('/blog/${blog._id');
 })
 
 
